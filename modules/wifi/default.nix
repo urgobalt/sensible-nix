5f46709b2412d64b7b421e9de8e6300872ef117f
Cindy <dante@nilsson.name>
2024-09-02 21:07:00 +0000

    Saving system

--- modules/wifi/default.nix
+++ modules/wifi/default.nix
@@ -7,40 +7,13 @@
 with lib; let
   cfg = config.modules.wifi;
 in {
-  options.modules.wifi = {enable = mkEnableOption "wifi";};
+  options.modules.wifi = {enable = mkEnableOption "wifi";networks=mkOption {type=types.attrs; default={}; description="wifi networks";};};
   config = mkIf cfg.enable {
     networking.wireless.enable = true;
     networking.networkmanager.enable = false;
 
     networking.wireless.userControlled.enable = true;
     networking.wireless.environmentFile = config.age.secrets.wifi-env.path;
-    networking.wireless.networks = {
-      "Spökhuset" = {
-        psk = "@PSK_SPOKHUSET@";
-        priority = 4;
-      };
-      "Nygren" = {
-        psk = "@PSK_NYGREN@";
-        priority = 5;
-      };
-      "Cinderblock" = {
-        psk = "@PSK_CINDERBLOCK@";
-        priority = 10;
-      };
-      "eduroam" = {
-        auth = ''
-          key_mgmt=WPA-EAP
-          eap=PEAP
-          identity="@I_EDUROAM@"
-          password="@PSK_EDUROAM@"
-          phase2="auth=MSCHAPV2"
-        '';
-        priority = 20;
-      };
-      "urgobalt" = {
-        psk = "@PSK_URGOBALT@";
-        priority = 99;
-      };
-    };
+    networking.wireless.networks = cfg.networks;
   };
 }
