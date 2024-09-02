8f34739895422f863d6a26f2c64df76152ea283f
Cindy Nilsson <cindynilsson@duck.com>
2024-09-02 12:00:47 +0200

    fix: adding pam_google_auth support

--- modules/display-manager/default.nix
+++ modules/display-manager/default.nix
@@ -8,7 +8,7 @@ with lib; let
   cfg = config.modules.display-manager;
   # regreet = import ./regreet.nix {inherit pkgs;};
 in {
-  options.modules.display-manager = {enable = mkEnableOption "display-manager";};
+  options.modules.display-manager = {enable = mkEnableOption "display-manager"; pam_google_auth = mkEnableOption "display-manager"};
   config = mkIf cfg.enable {
     # environment.systemPackages = with pkgs; [
     #   cage
@@ -38,5 +38,21 @@ in {
     };
 
     boot.plymouth.enable = true;
+    mkIf cfg.pam_google_auth{
+      security.pam.services.sddm = {
+        auth =  [
+      {
+        # Standard UNIX authentication
+        module = "pam_unix.so";
+        arguments = "try_first_pass";
+      }
+      {
+        # Google Authenticator module
+        module = "pam_google_authenticator.so";
+        arguments = "nullok";
+      }
+    ];
+  };
+    }
   };
 }
