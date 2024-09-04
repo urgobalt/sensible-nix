8d1bf9c8303f11b95e84d42cf50843e38bbcabf2
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-09-04 18:41:51 +0200

    fix: hyprkool

--- overlays/hyprkool.nix
+++ overlays/hyprkool.nix
@@ -1,37 +1,153 @@
 {
+  lib,
   stdenv,
-  fetchzip,
+  fetchFromGitHub,
+  pkgs,
+  gcc13Stdenv,
+  buildEnv,
+  pkg-config,
   cmake,
-  hyprland,
+  ninja,
+  cairo,
+  expat,
+  fribidi,
+  git,
+  hwdata,
+  hyprcursor,
+  hyprlang,
+  hyprutils,
+  libGL,
+  libdatrie,
+  libdisplay-info,
+  libdrm,
+  libexecinfo,
+  libinput,
+  libliftoff,
+  libselinux,
+  libsepol,
+  libthai,
+  libuuid,
+  libxkbcommon,
+  mesa,
+  pango,
+  pciutils,
+  pcre2,
+  seatd,
+  systemd,
+  tomlplusplus,
+  wayland,
+  wayland-protocols,
+  xorg,
+  xwayland,
   meson,
-}:
-stdenv.mkDerivation rec {
-  pname = "hyprkool";
-  version = "e0ae6c6b6513eaa70540fea200031743e4b62165";
-
-  src = fetchzip {
-    url = "https://github.com/thrombe/hyprkool/archive/refs/tags/0.7.1.tar.gz";
-    hash = "sha256-II9j5IplV6tWg7AKsKIGkFSeDby+AJgB+nNHv3cU16k=";
+  hyprland,
+  enableXWayland ? true,
+  withSystemd ? lib.meta.availableOn stdenv.hostPlatform systemd,
+}: let
+  meta = {
+    name = "hyprkool";
+    version = "3f56d829b44218d70867886b91d9f697d8ad9f68";
+    hash = "sha256-HHmJey9l4+BW+5PYMLfeqcHwtIZcJ8ZI42ARgRRNC+E=";
   };
+  binary = pkgs.unstable.rustPlatform.buildRustPackage rec {
+    pname = "${meta.name}-bin";
+    version = meta.version;
 
-  nativeBuildInputs = [
-    hyprland
-  ];
+    src = fetchFromGitHub {
+      owner = "thrombe";
+      repo = "hyprkool";
+      rev = meta.version;
+      sha256 = meta.hash;
+    };
+    cargoLock = {
+      lockFile = "${src}/Cargo.lock";
+      outputHashes = {
+        "hyprland-0.4.0-alpha.2" = "sha256-7GRj0vxsQ4ORp0hSBAorjFYvWDy+edGU2IL3DhFDLvQ=";
+      };
+    };
+    nativeBuildInputs = [
+      pkg-config
+    ];
+  };
+  plugin = gcc13Stdenv.mkDerivation rec {
+    pname = "${meta.name}-plugin";
+    version = meta.version;
 
-  buildInputs = [
-    cmake
-    meson
-  ];
+    src = fetchFromGitHub {
+      owner = "thrombe";
+      repo = "hyprkool";
+      rev = meta.version;
+      sha256 = meta.hash;
+    };
 
-  buildPhase = ''
-    make plugin
-    mv ./plugin/build/lib${pname}.so .
-  '';
-  installPhase = ''
-    mkdir -p $out/lib
-    mv ./lib${pname}.so $out/lib/lib${pname}.so
-  '';
+    nativeBuildInputs = [
+      pkg-config
+      hyprland
+    ];
 
-  dontUseCmakeConfigure = true;
-  dontUseMesonConfigure = true;
-}
+    buildInputs = lib.concatLists [
+      [
+        cairo
+        expat
+        fribidi
+        git
+        hwdata
+        hyprcursor
+        hyprlang
+        hyprutils
+        libdatrie
+        libdisplay-info
+        libdrm
+        libGL
+        libinput
+        libliftoff
+        libselinux
+        libsepol
+        libthai
+        libuuid
+        libxkbcommon
+        mesa
+        pango
+        pciutils
+        pcre2
+        seatd
+        tomlplusplus
+        wayland
+        wayland-protocols
+        xorg.libXcursor
+        ninja
+        cmake
+        meson
+      ]
+      (lib.optionals stdenv.hostPlatform.isMusl [libexecinfo])
+      (lib.optionals enableXWayland [
+        xorg.libxcb
+        xorg.libXdmcp
+        xorg.xcbutil
+        xorg.xcbutilerrors
+        xorg.xcbutilrenderutil
+        xorg.xcbutilwm
+        xwayland
+      ])
+      (lib.optionals withSystemd [systemd])
+    ];
+    dontUseMesonConfigure = true;
+    dontUseCmakeConfigure = true;
+    buildPhase = ''
+      make plugin
+      mv ./plugin/build/lib${meta.name}.so .
+    '';
+    installPhase = ''
+      mkdir -p $out/lib
+      mv ./lib${meta.name}.so $out/lib/lib${meta.name}.so
+    '';
+  };
+in
+  stdenv.mkDerivation {
+    pname = "hyprkool";
+    version = meta.version;
+    src = buildEnv {
+      name = "hyprkool-join";
+      paths = [binary plugin];
+    };
+  }
