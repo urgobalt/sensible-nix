faa952f75fa6d68163d5bc674610b8ac3d6296ba
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-09-04 18:18:04 +0200

    fix: hyprkool

--- overlays/hyprkool.nix
+++ overlays/hyprkool.nix
@@ -3,8 +3,9 @@
   fetchzip,
   cmake,
   hyprland,
+  meson,
 }:
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "hyprkool";
   version = "e0ae6c6b6513eaa70540fea200031743e4b62165";
 
@@ -12,6 +13,24 @@ stdenv.mkDerivation {
     url = "https://github.com/thrombe/hyprkool/archive/refs/tags/0.7.1.tar.gz";
     hash = "sha256-II9j5IplV6tWg7AKsKIGkFSeDby+AJgB+nNHv3cU16k=";
   };
+
+  nativeBuildInputs = [
+    hyprland
+  ];
+
+  buildInputs = [
+    cmake
+    meson
+  ];
+
+  buildPhase = ''
+    make plugin
+    mv ./plugin/build/lib${pname}.so .
+  '';
+  installPhase = ''
+    mkdir -p $out/lib
+    mv ./lib${pname}.so $out/lib/lib${pname}.so
+  '';
 
   dontUseCmakeConfigure = true;
   dontUseMesonConfigure = true;
