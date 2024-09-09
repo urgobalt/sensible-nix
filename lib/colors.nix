b023da7f2d0fe38a4f7f4f04e8fb04e143a3fe23
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-09-09 22:19:23 +0200

    fix: zero_prefix colors

--- lib/colors.nix
+++ lib/colors.nix
@@ -40,39 +40,39 @@
   # placing the alpha channel at the start
   zero_prefix = {
     background = "0xE41A1A19";
-    text = "0xD1D1D1";
+    text = "0xFFD1D1D1";
     /*
     Normal
     */
-    black = "0x333332";
-    red = "0xFF968C";
-    green = "0x61957F";
-    yellow = "0xFFC591";
-    blue = "0x8DB4D4";
-    magenta = "0xDE9BC8";
-    cyan = "0x7BB099";
-    white = "0xD1D1D1";
+    black = "0xFF333332";
+    red = "0xFFFF968C";
+    green = "0xFF61957F";
+    yellow = "0xFFFFC591";
+    blue = "0xFF8DB4D4";
+    magenta = "0xFFDE9BC8";
+    cyan = "0xFF7BB099";
+    white = "0xFFD1D1D1";
     /*
     Bright
     */
-    bright_black = "0x4C4C4B";
-    bright_red = "0xFFAFA5";
-    bright_green = "0x7AAE98";
-    bright_yellow = "0xFFDEAA";
-    bright_blue = "0xA6CDED";
-    bright_magenta = "0xF7B4E1";
-    bright_cyan = "0x94C9B2";
-    bright_white = "0xEAEAEA";
+    bright_black = "0xFF4C4C4B";
+    bright_red = "0xFFFFAFA5";
+    bright_green = "0xFF7AAE98";
+    bright_yellow = "0xFFFFDEAA";
+    bright_blue = "0xFFA6CDED";
+    bright_magenta = "0xFFF7B4E1";
+    bright_cyan = "0xFF94C9B2";
+    bright_white = "0xFFEAEAEA";
     /*
     Grays
     */
-    gray01 = "0x222221";
-    gray02 = "0x2A2A29";
-    gray03 = "0x323231";
-    gray04 = "0x3A3A39";
-    gray05 = "0x6A6A69";
-    gray06 = "0x767675";
-    gray07 = "0xB6B6B5";
+    gray01 = "0xFF222221";
+    gray02 = "0xFF2A2A29";
+    gray03 = "0xFF323231";
+    gray04 = "0xFF3A3A39";
+    gray05 = "0xFF6A6A69";
+    gray06 = "0xFF767675";
+    gray07 = "0xFFB6B6B5";
 
     # None
     none = "0x00000000";
