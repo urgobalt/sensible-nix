bddd13960342acb3b107b489364100e254c8884d
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-09-13 19:09:38 +0200

    fix: added a new background and formatted the default.nix file

--- assets/Blur-Glassy-V.2/default.nix
+++ assets/Blur-Glassy-V.2/default.nix
@@ -1,42 +1,34 @@
 {
   stdenvNoCC,
   pkgs,
-  name
-}:
-
-let
-  KdeInput=with pkgs.kdePackages;[
-    plasma-workspace
-    ];
-  buildInput=with pkgs.libsForQt5;[
+  name,
+}: let
+  propegatedBuildInputs = with pkgs.libsForQt5; [
     qt5.qtgraphicaleffects
     plasma-workspace
     plasma-framework
     kconfig
-
-  ]++KdeInput;
-
+  ];
 in
   stdenvNoCC.mkDerivation {
-  name = "sddm-theme";
-  src = ./.;
+    name = "sddm-theme";
+    src = ./.;
 
-  propegatedBuildInputs = buildInput;
+    propegatedBuildInputs = propegatedBuildInputs;
 
-  dontWrapQtApps = true;
+    dontWrapQtApps = true;
 
-  installPhase = ''
-    mkdir -p $out/share/sddm/themes
-    cp -r $src $out/share/sddm/themes/${name}
+    installPhase = ''
+      mkdir -p $out/share/sddm/themes
+      cp -r $src $out/share/sddm/themes/${name}
 
-    mkdir -p $out/nix-support
+      mkdir -p $out/nix-support
     '';
 
-  postFixup = with pkgs.libsForQt5;''
-    export support=$out/nix-support/propagated-user-env-packages
-    echo ${qt5.qtgraphicaleffects} >> $support
-    echo ${plasma-workspace}>> $support
-    echo ${plasma-framework}>>$support
-    echo ${kconfig}>>$support
+    postFixup = with pkgs.libsForQt5; ''
+      echo ${qt5.qtgraphicaleffects} >> $out/nix-support/propagated-user-env-packages
+      echo ${plasma-workspace} >> $out/nix-support/propagated-user-env-packages
+      echo ${plasma-framework} >> $out/nix-support/propagated-user-env-packages
+      echo ${kconfig} >> $out/nix-support/propagated-user-env-packages
     '';
-}
+  }
