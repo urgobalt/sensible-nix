b6f24b40140c107a7c7d84b3156c51508b1a3b81
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-10-07 11:29:19 +0200

    feat: sddm enabled and configured with working themes

--- modules/display-manager/default.nix
+++ modules/display-manager/default.nix
@@ -10,36 +10,47 @@ with lib; let
   cfg = config.modules.display-manager;
 in {
   options.modules.display-manager = {
-    enable = mkEnableOption "display-manager";
-    # pam_google_auth = mkOption {
-    #   type = types.bool;
-    #   default = false;
-    #   description = "Enable pam_2fa with google authenticator";
-    # };
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable the sddm display manager.";
+    };
+    theme = {
+      package = mkOption {
+        type = types.package;
+        default = pkgs.sddm-glassy;
+        description = "The package that contain the theme.";
+      };
+      name = mkOption {
+        type = types.str;
+        default = "sddm-glassy";
+        description = "The name of the package. It is the name of the folder within /share/sddm/themes.";
+      };
+      extraPackages = mkOption {
+        type = with types; listOf package;
+        default = with pkgs.libsForQt5; [
+          qt5.qtgraphicaleffects
+          plasma-workspace
+          plasma-framework
+          kconfig
+        ];
+        description = "Extra Qt plugins and/or QML libraries to add to the environment.";
+      };
+    };
   };
   config =
     mkIf cfg.enable
     {
-      environment.systemPackages = with pkgs; [greetd.regreet];
+      environment.systemPackages = [cfg.theme.package];
       programs.hyprland = {
         enable = true;
         xwayland.enable = true;
       };
-      environment.etc = {
-        "greetd/regreet.toml".source = import ./regreet.nix {inherit pkgs wallpaper;};
-        # We use a home manager generator to define the hyprland configuration.
-        # We need to define the text field instead of a file.
-        "greetd/hyprland.conf".text = import ./hyprland.nix {inherit pkgs lib wallpaper;};
-      };
-      services.greetd = {
+      services.displayManager.sddm = {
         enable = true;
-        settings = {
-          default_session = {
-            command = "${lib.getExe config.programs.hyprland.package} --config /etc/greetd/hyprland.conf";
-            # command = "${lib.getExe pkgs.cage} regreet";
-            user = user;
-          };
-        };
+        extraPackages = cfg.theme.extraPackages;
+        theme = cfg.theme.name;
+        wayland.enable = true;
       };
       boot.plymouth.enable = true;
     };
