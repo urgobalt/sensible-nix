65797c560b571da09253a7656200c45f1c679361
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-10-07 12:54:03 +0200

    fix: added back greetd as an optional display manager

--- modules/display-manager/default.nix
+++ modules/display-manager/default.nix
@@ -15,6 +15,11 @@ in {
       default = false;
       description = "Enable the sddm display manager.";
     };
+    greeter = mkOption {
+      type = with types; enum ["sddm" "greetd"];
+      default = "sddm";
+      description = "The greeter that will be used.";
+    };
     autologin = {
       enable = mkOption {
         type = types.bool;
@@ -43,21 +48,23 @@ in {
         ];
         description = "Extra Qt plugins and/or QML libraries to add to the environment.";
       };
-      autoNumlock = mkOption {
-        type = types.bool;
-        default = true;
-        description = "Enable numlock at login";
-      };
+    };
+    autoNumlock = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable numlock at login";
     };
   };
-  config =
-    mkIf cfg.enable
+  config = mkIf cfg.enable (mkMerge [
     {
       environment.systemPackages = [cfg.theme.package];
       programs.hyprland = {
         enable = true;
         xwayland.enable = true;
       };
+      boot.plymouth.enable = true;
+    }
+    (mkIf (cfg.greeter == "sddm") {
       services.displayManager.sddm = {
         enable = true;
         extraPackages = cfg.theme.extraPackages;
@@ -65,6 +72,24 @@ in {
         wayland.enable = true;
         autoNumlock = cfg.autoNumlock;
       };
-      boot.plymouth.enable = true;
-    };
+    })
+    (mkIf (cfg.greeter == "greetd") {
+      environment.etc = {
+        "greetd/regreet.toml".source = import ./regreet.nix {inherit pkgs wallpaper;};
+        # We use a home manager generator to define the hyprland configuration.
+        # We need to define the text field instead of a file.
+        "greetd/hyprland.conf".text = import ./hyprland.nix {inherit pkgs lib wallpaper;};
+      };
+      services.greetd = {
+        enable = true;
+        settings = {
+          default_session = {
+            command = "${lib.getExe config.programs.hyprland.package} --config /etc/greetd/hyprland.conf";
+            # command = "${lib.getExe pkgs.cage} regreet";
+            user = user;
+          };
+        };
+      };
+    })
+  ]);
 }
