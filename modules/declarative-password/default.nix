56ac24963a7b4ba9974ea614bbadd98fbc06fe4f
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-12-25 13:18:48 +0100

    fix: removed the path check inside the declarative password

--- modules/declarative-password/default.nix
+++ modules/declarative-password/default.nix
@@ -7,18 +7,16 @@
 }:
 with lib; let
   cfg = config.modules.declarative-password;
-  path = config.age.secrets.user-password.path;
-  path_exist = pathExists path;
 in {
   options.modules.declarative-password = {enable = mkEnableOption "declarative-password";};
   config = mkMerge [
-    (mkIf (cfg.enable && path_exist) {
+    (mkIf (cfg.enable) {
       users.mutableUsers = false;
 
       users.users.${user}.hashedPasswordFile = path;
     })
-    (mkIf (cfg.enable == false || path_exist == false) {
-      users.users.${user}.initialPassword = "root";
+    (mkIf (cfg.enable == false) {
+      users.users.${user}.password = "root";
     })
   ];
 }
