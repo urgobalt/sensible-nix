23af666bac28403484e104188bf5a74c3a141762
Urgobalt <ludvigkallqvistnygren@gmail.com>
2025-02-24 19:41:20 +0100

    fix: swaynotificationmanager configuration init - added a shortcut to hyprland to open the notification tray - added config.json in .config/swaync for configuring the notification   daemon - added style.css in .config/swaync for styling the notification daemon - changed the colorscheme to match

--- home/swaync/default.nix
+++ home/swaync/default.nix
@@ -2,13 +2,18 @@
   pkgs,
   lib,
   config,
+  colors,
   ...
 }:
 with lib; let
   cfg = config.modules.swaync;
+  c = colors.rgba;
 in {
   options.modules.swaync = {enable = mkEnableOption "swaync";};
   config = mkIf cfg.enable {
     home.packages = with pkgs; [swaynotificationcenter];
+
+    xdg.configFile."swaync/config.json".text = import ./config.nix;
+    xdg.configFile."swaync/style.css".text = import ./style.nix {color = c;};
   };
 }
