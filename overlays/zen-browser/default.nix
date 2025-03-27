f19be441395e3da5cd6196ca7d63121b6171f0f1
Cindy Nilsson <cindy.a.nilsson@gmail.com>
2025-03-27 15:27:39 +0100

    fix: Upgrading zen-browser

--- overlays/zen-browser/default.nix
+++ overlays/zen-browser/default.nix
@@ -57,11 +57,11 @@
     ]);
 in
   pkgs.stdenv.mkDerivation rec {
-    version = "1.8.2b";
+    version = "1.10.2b";
     pname = "zen-browser";
     src = builtins.fetchTarball {
       url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-      sha256 = "sha256:0lqsspqnrp2v6ca6yacx7aq6afqr092q60aas1q6p7p15ky9ip7i";
+      sha256 = "sha256:1z86b4cf84q517nb1kybafa20pb77a6vswsf6wl2gw85hxwb5yrp";
     };
 
     desktopSrc = ./.;
