fa13f3e7edbea83a1ec931cde2d4431db08eea9e
Cindy Nilsson <cindy.a.nilsson@gmail.com>
2025-07-02 23:30:56 +0200

    J: Enter a description for the selected changes. flake update

--- home/hyprland/hypridle.nix
+++ home/hyprland/hypridle.nix
@@ -28,10 +28,5 @@
       on-timeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed
       on-resume = "hyprctl dispatch dpms on && brightnessctl -r"; # screen on when activity is detected after timeout has fired.
     }
-
-    {
-      timeout = 1800; # 30min
-      on-timeout = "systemctl suspend"; # suspend pc
-    }
   ];
 }
