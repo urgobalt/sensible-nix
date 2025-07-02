fccc0aa817325a6951d012b940aec54e71934959
Cindy Nilsson <cindy.a.nilsson@gmail.com>
2025-07-02 23:30:56 +0200

    fixing discord update forced

--- home/social/default.nix
+++ home/social/default.nix
@@ -11,6 +11,15 @@ in {
     enable = mkEnableOption "social";
   };
   config = mkIf cfg.enable {
+    home.file.".config/discord/settings.json" = {
+      # The `text` attribute takes a string, which we generate from a Nix attribute set.
+      text = builtins.toJSON {
+        SKIP_HOST_UPDATE = true;
+        # You can add other Discord settings here as needed.
+        # For example, to prevent it from starting on boot:
+        OPEN_ON_STARTUP = false;
+      };
+    };
     home.packages = with pkgs; [
       unstable.discord
       unstable.discord-canary
