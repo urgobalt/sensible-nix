957685739d0d4ece22ab6b402f1d30840a2a2e4b
Urgobalt <ludvigkallqvistnygren@gmail.com>
2025-08-13 15:36:02 +0200

    stylix because I could not be bothered to wait for rewrite

--- home/fuzzel/default.nix
+++ home/fuzzel/default.nix
@@ -13,20 +13,20 @@ in {
   config = mkIf cfg.enable {
     programs.fuzzel = {
       enable = true;
-      settings = {
-        colors = {
-          background = c.background;
-          text = c.text;
-          match = "7bb099ff";
-          selection = "2a2a29ff";
-          selection-text = "d1d1d1ff";
-          selection-match = "7bb099ff";
-          border = "ffdeaaff";
-        };
-        border = {
-          width = 4;
-        };
-      };
+      # settings = {
+      #   colors = {
+      #     background = c.background;
+      #     text = c.text;
+      #     match = "7bb099ff";
+      #     selection = "2a2a29ff";
+      #     selection-text = "d1d1d1ff";
+      #     selection-match = "7bb099ff";
+      #     border = "ffdeaaff";
+      #   };
+      #   border = {
+      #     width = 4;
+      #   };
+      # };
     };
   };
 }
